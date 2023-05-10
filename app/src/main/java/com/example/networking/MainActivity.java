b279b7d2331f47cbd1187e2311d4f7713366ffc8
Lina H <f22linhe@student.his.se>
2023-05-10 20:03:56 +0200

    Lade till adapter, kopplade adaptern till min recyclerview samt lade till ytterligare kod för avkodning från json till gson.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.networking;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("FieldCanBeLocal")
@@ -31,8 +35,22 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         Gson gson = new Gson();
         Type type = new TypeToken<List<Mountain>>() {}.getType();
         List<Mountain> ListOfMountains = gson.fromJson(json, type);
+        ArrayList<Mountain> items = new ArrayList<>();
 
         for (Mountain berg : ListOfMountains) {
             Log.d("Mountain", berg.toString());
+            items.add(new Mountain(berg.getName()));
     }
-}}
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(Mountain item) {
+                Toast.makeText(MainActivity.this, item.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        RecyclerView recycler_view = findViewById(R.id.recycler_view);
+        recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        recycler_view.setAdapter(adapter);
+    }
+}
